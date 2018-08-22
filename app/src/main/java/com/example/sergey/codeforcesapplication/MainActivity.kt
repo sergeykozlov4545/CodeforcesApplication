e25706ed36bd8381fefbdc79e2730683b3b02de6
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-08-22 17:34:17 +0500

    верска экрана списка контестов

--- app/src/main/java/com/example/sergey/codeforcesapplication/MainActivity.kt
+++ app/src/main/java/com/example/sergey/codeforcesapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.sergey.codeforcesapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (toolbar.findViewById<TextView>(R.id.title_toolbar)).setText(R.string.contestsList)
     }
 }
