3b98af235c33061af5533408b296b837f3f83a9d
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-08-23 18:02:09 +0500

    Добавление ServiceApi

---
+++ app/src/main/java/com/example/sergey/codeforcesapplication/model/remote/ServiceApi.kt
@@ -0,0 +1,12 @@
+package com.example.sergey.codeforcesapplication.model.remote
+
+import com.example.sergey.codeforcesapplication.model.Contest
+import kotlinx.coroutines.experimental.Deferred
+import retrofit2.http.GET
+
+interface ServiceApi {
+
+    @GET("/contest.list")
+    fun getContestsList(): Deferred<Response<Contest>>
+
+}
