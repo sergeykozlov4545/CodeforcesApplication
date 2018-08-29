c5fa3f7a6a9b3bb37bd6a1a7dce6f60959da4d0a
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-08-29 15:12:57 +0500

    Добавление полей в User

--- app/src/main/java/com/example/sergey/codeforcesapplication/model/pojo/User.kt
+++ app/src/main/java/com/example/sergey/codeforcesapplication/model/pojo/User.kt
@@ -3,6 +3,12 @@ package com.example.sergey.codeforcesapplication.model.pojo
 import com.google.gson.annotations.SerializedName
 
 data class User(
+        @SerializedName("avatar")
+        val avatarUrl: String,
+
+        @SerializedName("titlePhoto")
+        val titlePhoto: String,
+
         @SerializedName("handle")
         val handle: String,
 
@@ -12,8 +18,41 @@ data class User(
         @SerializedName("lastName")
         val lastName: String?,
 
-        @SerializedName("avatar")
-        val avatarUrl: String
+        @SerializedName("country")
+        val country: String?,
+
+        @SerializedName("city")
+        val city: String?,
+
+        @SerializedName("registrationTimeSeconds")
+        val registrationTimeSeconds: Long,
+
+        @SerializedName("email")
+        val email: String?,
+
+        @SerializedName("vkId")
+        val vkId: String?,
+
+        @SerializedName("openId")
+        val openId: String?,
+
+        @SerializedName("friendOfCount")
+        val friendOfCount: Int,
+
+        @SerializedName("contribution")
+        val contribution: Int,
+
+        @SerializedName("rank")
+        val rank: String,
+
+        @SerializedName("rating")
+        val rating: Int,
+
+        @SerializedName("maxRank")
+        val maxRank: String,
+
+        @SerializedName("maxRating")
+        val maxRating: Int
 ) {
     val fullName: String
         get() {
