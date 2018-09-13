5ef66c32f9e6c4d71d7d01ae7aff820b306894b4
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-09-13 14:44:12 +0500

    Изменение работы DataContainer'ами

--- app/src/main/java/com/example/sergey/codeforcesapplication/feature/base/tabbed/TabbedActivity.kt
+++ app/src/main/java/com/example/sergey/codeforcesapplication/feature/base/tabbed/TabbedActivity.kt
@@ -3,7 +3,8 @@ package com.example.sergey.codeforcesapplication.feature.base.tabbed
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.TabLayout
-import android.support.v4.view.ViewPager
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import com.example.sergey.codeforcesapplication.R
 import com.example.sergey.codeforcesapplication.feature.base.BaseActivity
 import com.example.sergey.codeforcesapplication.feature.main.activityFactory.TabInfo
@@ -11,16 +12,68 @@ import com.example.sergey.codeforcesapplication.feature.main.activityFactory.Tab
 @SuppressLint("Registered")
 abstract class TabbedActivity : BaseActivity() {
 
-    private lateinit var tabLayout: TabLayout
-    private lateinit var contentPager: ViewPager
-
+    private var tabPosition: Int = 0
     abstract val tabsInfo: List<TabInfo>
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        contentPager = findViewById(R.id.contentPager)
-        contentPager.adapter = PagerAdapter(supportFragmentManager, tabsInfo)
-        tabLayout = findViewById(R.id.tabsLayout)
-        tabLayout.setupWithViewPager(contentPager)
+
+        restoreData(savedInstanceState)
+        initTabs()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.apply {
+            putInt(TAB_POSITION_EXTRA, tabPosition)
+        }
+    }
+
+    private fun restoreData(savedInstanceState: Bundle?) {
+        tabPosition = savedInstanceState?.getInt(TAB_POSITION_EXTRA, 0) ?: 0
+    }
+
+    private fun initTabs() {
+        val tabLayout = findViewById<TabLayout>(R.id.tabsLayout)
+        tabsInfo.forEach { info ->
+            tabLayout.addTab(tabLayout.newTab().apply {
+                text = info.tabTitle
+            })
+        }
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tabPosition = tab?.position ?: return
+                showContentTab(tabPosition)
+            }
+        })
+
+        if (tabPosition == 0) {
+            showContentTab(tabPosition)
+        } else {
+            tabLayout.getTabAt(tabPosition)?.select()
+        }
+    }
+
+    private fun showContentTab(tabPosition: Int) {
+        showFragment(tabsInfo[tabPosition].fragment)
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        // TODO: Использовать KTX
+        supportFragmentManager.beginTransaction().apply {
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            replace(R.id.content, fragment)
+        }.commitNowAllowingStateLoss()
+    }
+
+    companion object {
+        private const val TAB_POSITION_EXTRA = "tab_position"
     }
 }
